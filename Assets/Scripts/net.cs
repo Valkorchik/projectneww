2cf92f6d1178472cb760176b537eeba49d1c8050
Nikita <nikita2003.ns@gmail.com>
2020-01-11 17:52:05 +0300

    gg

---
+++ Assets/Scripts/net.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class net : MonoBehaviour
+{
+    public void prosmotrsite()
+    {
+        SceneManager.LoadScene("button2");
+    }
+    public void mirea()
+    {
+        Application.OpenURL("https://www.ikea.com/ru/ru/?utm_source=yandex&utm_medium=cpc&utm_campaign=ipr_Msk_image_name_nonpure&utm_content=pid|0100000017118390131_|cid|37137827|gid|3860413041|aid|7646378933|pos|premium1|key|%D0%B8%D0%BA%D0%B5%D0%B0|addphrases|no|dvc|desktop&utm_term=%D0%B8%D0%BA%D0%B5%D0%B0&_openstat=ZGlyZWN0LnlhbmRleC5ydTszNzEzNzgyNzs3NjQ2Mzc4OTMzO3lhbmRleC5ydTpwcmVtaXVt&yclid=18253847847808826054");
+    }
+}
---
+++ Assets/Scripts/net2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class net2 : MonoBehaviour
+{
+    public string url;
+
+    public void OnButtomUrlClick()
+    {
+        Application.OpenURL(url);
+    }
+}
